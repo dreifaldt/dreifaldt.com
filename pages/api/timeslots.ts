81ce736b51afb66525f8719b4786df9d88258c16
Erik Dreifaldt <erik.dreifaldt@gmail.com>
2022-10-31 21:54:08 +0100

    add 'public/timeslots' to next api func

--- pages/api/timeslots.ts
+++ pages/api/timeslots.ts
@@ -9,9 +9,11 @@ export const getTimeSlots = async (
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) => {
-  const response = await fetch(`${process.env.API_HOST}/ping`)
+  const response = await fetch(
+    `${process.env.API_HOST}/public/timeslots?bookingType=MIDWIFE`
+  )
   const data = await response.json()
-  return new Response(JSON.stringify({ ...data }), {
+  return new Response(JSON.stringify({ data }), {
     status: 200,
     headers: {
       'Cache-Control': 'max-age=0, s-maxage=3600, stale-while-revalidate'
