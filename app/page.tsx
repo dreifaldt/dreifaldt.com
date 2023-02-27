9650e3d5978d96d587bd661ca92648bf58e4c121
Erik Dreifaldt <erik.dreifaldt@gmail.com>
2023-02-27 22:01:00 +0100

    add text to page

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import { Suspense } from 'react'
 import styled from 'styled-components'
 import Loading from './loading'
 import { theme } from '../theme/theme'
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -12,6 +13,14 @@ export default function Home() {
         <Span>
           <h1>&lt; DREIFALDT &gt;</h1>
         </Span>
+        <div>
+          Information regarding previous endevours can be found on my LinkedIn.
+          <br />
+          Interested in working with me?
+          <Link href="mailto:erik@dreifaldt.com">Let me know by email</Link>.<br />
+          Have a great day,
+          <br />— Erik
+        </div>
       </Suspense>
     </Grid>
   )
@@ -23,10 +32,33 @@ const Grid = styled.div`
   bottom: 0;
   right: 0;
   left: 0;
+  background-color: ${theme.colors.black};
   display: flex;
+  flex: 1;
   align-items: center;
   justify-content: center;
   flex-direction: column;
+
+  div {
+    color: ${theme.colors.white};
+    font-size: 24px;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans',
+      'Helvetica Neue', sans-serif;
+
+    .blue-underline {
+      text-decoration: underline;
+      text-decoration-color: #3785f7;
+    }
+
+    .orange-underline {
+      text-decoration: underline;
+      text-decoration-color: #f3a23c;
+    }
+
+    .grey {
+      color: #1e1e1e;
+    }
+  }
 `
 
 const Span = styled.span`
@@ -38,7 +70,7 @@ const Span = styled.span`
     text-align: center;
     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans',
       'Helvetica Neue', sans-serif;
-    color: white;
+    color: ${theme.colors.white};
     font-size: 60px;
 
     @media (min-width: ${theme.breakpoints.small}px) {
@@ -47,6 +79,10 @@ const Span = styled.span`
 
     @media (min-width: ${theme.breakpoints.large}px) {
       font-size: 140px;
+    }
+
+    :hover {
+      cursor: pointer;
     }
   }
 `
