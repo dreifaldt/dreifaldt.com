1f221c904af5049e8dfc5344dde22d54d9127480
eridr <erik@dreifaldt.com>
2023-11-25 22:35:43 +0100

    Refactor message component to display links as clickable URLs

--- src/components/message/index.tsx
+++ src/components/message/index.tsx
@@ -8,16 +8,22 @@ interface MessageProps {
 }
 
 export const Message: FC<MessageProps> = ({ text, sender }) => {
-  const newText = `${text} https://surfers.se/kitesurfing/kites/cabrinha-kites/cabrinha-kite-moto-x-2023 `
-  const links = getHttpsLinks(newText) || []
+  const links = getHttpsLinks(text) || []
 
   return (
     <div className={`flex flex-col gap-4 py-4 px-4`}>
       <p className="font-bold text-sm text-gray-700">{sender}</p>
       <p>{text}</p>
+
       {links.map((link, index) => (
-        <a key={index} href="#" style={{ display: 'block', fontSize: 'small' }}>
-          {link}
+        <a
+          key={index}
+          href={link}
+          className="text-blue-500 font-bold text-xs"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Länk {index + 1}
         </a>
       ))}
     </div>
