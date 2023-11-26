f952dc8fb0c8bc4ce5e6f3744cc93143fd06265c
eridr <erik@dreifaldt.com>
2023-11-26 11:36:01 +0100

    Refactor code to remove unused variables and comments

--- src/app/bot/page.tsx
+++ src/app/bot/page.tsx
@@ -20,70 +20,70 @@ export default function Bot() {
   //   object: 'thread',
   // }
 
-  const { data: thread, error } = queries.useGetThread()
+  // const { data: thread } = queries.useGetThread()
 
-  useMixpanel(thread)
+  // useMixpanel(thread)
 
-  const [conversation, setConversation] = useState<{ role: ThreadMessage['role']; text: string }[]>([])
+  // const [conversation, setConversation] = useState<{ role: ThreadMessage['role']; text: string }[]>([])
 
-  const [question, setQuestion] = useState('')
-  const [isLoading, setLoading] = useState(false)
+  // const [question, setQuestion] = useState('')
+  // const [isLoading, setLoading] = useState(false)
 
-  const onChange = (event: ChangeEvent<HTMLInputElement>) => {
-    setQuestion(event.target.value)
-  }
+  // const onChange = (event: ChangeEvent<HTMLInputElement>) => {
+  //   setQuestion(event.target.value)
+  // }
 
-  const pollingRunStatus = async (thread: Thread, run: Run) => {
-    const response = await api.checkRunStatus(thread, run)
+  // const pollingRunStatus = async (thread: Thread, run: Run) => {
+  //   const response = await api.checkRunStatus(thread, run)
 
-    if (response.status === 'completed') {
-      const response: ThreadMessagesPage = await api.getResponse(thread)
+  //   if (response.status === 'completed') {
+  //     const response: ThreadMessagesPage = await api.getResponse(thread)
 
-      const parsedConversation = response.data
-        .map((message) => {
-          const text = isMessageContentText(message.content[0]) ? message.content[0].text.value : ''
-          return { role: message.role, text }
-        })
-        .reverse()
+  //     const parsedConversation = response.data
+  //       .map((message) => {
+  //         const text = isMessageContentText(message.content[0]) ? message.content[0].text.value : ''
+  //         return { role: message.role, text }
+  //       })
+  //       .reverse()
 
-      setConversation(parsedConversation)
-      return
-    } else if (response.status === 'requires_action') {
-      alert('requires action')
-    } else if (response.status === 'in_progress') {
-      pollingRunStatus(thread, run)
-    } else if (response.status === 'queued') {
-      pollingRunStatus(thread, run)
-    }
-  }
+  //     setConversation(parsedConversation)
+  //     return
+  //   } else if (response.status === 'requires_action') {
+  //     alert('requires action')
+  //   } else if (response.status === 'in_progress') {
+  //     pollingRunStatus(thread, run)
+  //   } else if (response.status === 'queued') {
+  //     pollingRunStatus(thread, run)
+  //   }
+  // }
 
-  const onSubmit = async (event: FormEvent<HTMLButtonElement | HTMLInputElement | HTMLFormElement>) => {
-    event.preventDefault()
+  // const onSubmit = async (event: FormEvent<HTMLButtonElement | HTMLInputElement | HTMLFormElement>) => {
+  //   event.preventDefault()
 
-    if (!thread) return
-    setLoading(true)
+  //   if (!thread) return
+  //   setLoading(true)
 
-    await api.addMessageToThread(thread, question)
+  //   await api.addMessageToThread(thread, question)
 
-    setConversation((prevValue) => [...prevValue, { role: 'user', text: question }])
-    setQuestion('')
+  //   setConversation((prevValue) => [...prevValue, { role: 'user', text: question }])
+  //   setQuestion('')
 
-    const run = await api.runAssistant(thread)
-    await pollingRunStatus(thread, run)
-    setLoading(false)
-  }
+  //   const run = await api.runAssistant(thread)
+  //   await pollingRunStatus(thread, run)
+  //   setLoading(false)
+  // }
 
   return (
     <Suspense fallback={<Loading />}>
       <main>
         {/* <h1>bot page</h1> */}
         <div className="flex flex-col justify-end h-screen bg-gray-100 w-full">
-          {conversation.map((message, index) => {
+          {/* {conversation.map((message, index) => {
             const sender = message.role === 'assistant' ? 'Snygg-Per' : 'User'
             return <Message key={index} sender={sender} text={message.text} />
           })}
 
-          <Input value={question} onChange={onChange} onSubmit={onSubmit} isLoading={isLoading} />
+          <Input value={question} onChange={onChange} onSubmit={onSubmit} isLoading={isLoading} /> */}
         </div>
       </main>
     </Suspense>
