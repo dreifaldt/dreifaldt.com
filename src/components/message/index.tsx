4e213b5f2534d82c33f28037e12d10a322110d7f
eridr <erik@dreifaldt.com>
2023-12-03 16:55:04 +0100

    Refactor Markdown rendering `<a>` in Message component

--- src/components/message/index.tsx
+++ src/components/message/index.tsx
@@ -10,7 +10,14 @@ export const Message: FC<MessageProps> = ({ text, sender }) => {
   return (
     <div className={`flex flex-col gap-4 py-4 px-4`}>
       <p className="font-bold text-sm text-gray-700">{sender}</p>
-      <Markdown className="text-blue-950">{text}</Markdown>
+      <Markdown
+        components={{
+          a: ({ node, ...props }) => <a {...props} className="text-blue-600 font-bold" />,
+        }}
+        className="text-blue-950"
+      >
+        {text}
+      </Markdown>
     </div>
   )
 }
