0831c1f901a81e35b8e15aaed15c5da595c17147
Erik Dreifaldt <erik@dreifaldt.com>
2024-09-16 19:07:37 +0200

    Update Terms of Service text for mobile application

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -33,7 +33,7 @@ export default function Home() {
               -&gt;
             </span>
           </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>Terms of service for the application mobile Saga.</p>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>Terms of service for the mobile application Saga.</p>
         </Link>
         <Link
           href="/integrity"
