8e6a5c9be5c8b651ca9a444cd405c7e2ba1b374b
Erik Dreifaldt <erik@dreifaldt.com>
2024-09-16 19:06:36 +0200

    Create `Integrity` page :wrench:

---
+++ src/app/integritet/page.tsx
@@ -0,0 +1,6 @@
+// Create a new page component for the privacy policy
+// It should facility for people to remove their data from the internet.
+
+export default function Integrity() {
+  return <h1>Integritet</h1>
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -33,7 +33,19 @@ export default function Home() {
               -&gt;
             </span>
           </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>Get information about kite surf equipment.</p>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>Terms of service for the application mobile Saga.</p>
+        </Link>
+        <Link
+          href="/integrity"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+        >
+          <h2 className={`mb-3 text-2xl font-semibold`}>
+            Integrity{' '}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>Protect your integrity on the internet.</p>
         </Link>
       </div>
     </main>
