b224f2cd37e8a756787b4b24b96a3e1d7709fd06
Erik Dreifaldt <erik@dreifaldt.com>
2024-09-16 19:14:52 +0200

    Rename page from `Integritet` to `Integrity`

---
+++ src/app/integrity/page.tsx
@@ -0,0 +1,14 @@
+// Create a new page component for the privacy policy
+// It should facility for people to remove their data from the internet.
+
+export default function Integrity() {
+  return (
+    <div>
+      <h1>Integritet</h1>
+      <form action="/search">
+        <input name="query" />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  )
+}
