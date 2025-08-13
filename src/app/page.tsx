06b84a654ab53232ec10c4b08905e39f6c2354dd
Erik Dreifaldt <erik.dreifaldt@gmail.com>
2025-08-13 23:22:05 +0200

    refactor: update LinkedIn link and replace ToS with LinkedIn profile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -35,18 +35,20 @@ export default function Home() {
         </div>
 
         <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-          <Link
-            href="/tos"
+          <a
+            href="https://www.linkedin.com/in/erik-dreifaldt-293a0795/"
+            target="_blank"
+            rel="noopener noreferrer"
             className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           >
             <h2 className={`mb-3 text-2xl font-semibold`}>
-              ToS Saga{' '}
+              LinkedIn{' '}
               <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
                 -&gt;
               </span>
             </h2>
-            <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>Terms of service for the mobile application Saga.</p>
-          </Link>
+            <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>View LinkedIn profile.</p>
+          </a>
           <Link
             href="/integrity"
             className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
